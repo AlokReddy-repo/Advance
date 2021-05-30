c13d2bd54daad979d003c56fa9f103c672390eac
AlokReddy-repo <83569955+AlokReddy-repo@users.noreply.github.com>
2021-05-30 10:51:09 +0530

    new

--- template/temp.js
+++ template/temp.js
@@ -3,5 +3,15 @@
 
 $(document).ready(function(){
 	console.log($(this));
+
+	//// traveesing DOM elements
+	$("section").last().css('background-color','blue');
+	$('li').last().css('background-color','yellow');
+	$('#myframe').contents().find('.col-md-6').css('background-color','green');
+	$('#arrayme').click(function(){
+		console.log($('.one').children().toArray());
+	}); 
+	
+	$('.one').find('.four').css('border',' 2px dotted green');
 });
 /* eslint-enable no-console */
